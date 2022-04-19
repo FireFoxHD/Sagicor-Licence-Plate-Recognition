c92152aa2dabd6eb512e2b670c33d262cdf3a534
FireFoxHD <khamalipowell@gmail.com>
2022-04-19 01:27:32 -0500

    working with aws url

--- app.js
+++ app.js
@@ -15,17 +15,15 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-const API_KEY ='sk_f4c4c7031d8d45dba323a002';
-var url = "https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&secret_key=" + API_KEY;
+const secret_key ='sk_49a41bf53b3e366e4826f51d';
+let imgUrl = 'https://number-plate-recognition.s3.amazonaws.com/A-License-Plates-Lead-Image.jpg'
+let url = `https://api.openalpr.com/v3/recognize_url?image_url=${imgUrl}&recognize_vehicle=0&country=us&return_image=0&topn=10&secret_key=${secret_key}`;
 
-let base64String = base64_encode(path.join(__dirname,'./test_images/img-1.jpg'));
-axios.post(url,base64String)
+axios.post(url)
   .then(function (response) {
-    // handle success
-    console.log(response.json());
+    console.log(response.data.results[0].plate);
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
   });
 
@@ -33,8 +31,8 @@ axios.post(url,base64String)
 
 
 
-var server = app.listen(3000, () => {
-   var host = server.address().address
-   var port = server.address().port
-   console.log(`Example app listening at http://${host}:${port}`);
+let server = app.listen(3000, () => {
+  let host = server.address().address
+  let port = server.address().port
+  console.log(`Example app listening at http://${host}:${port}`);
 })
