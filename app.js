8eda664371e0f6fd866000b52728c2ad6a5388d6
FireFoxHD <khamalipowell@gmail.com>
2022-04-27 14:09:21 -0500

    added AWS integration

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
+require('dotenv').config()
 const express = require('express');
 const axios = require('axios').default;
 const fs = require('fs');
 const path = require ('path');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' })
-require('dotenv').config()
-const mongoose = require ('mongoose');
+const {uploadFile, getFileStream} = require('./s3Client')
 
 let app = express();
 
 //form input needs the name 'file' to work
 //Don't forget the enctype="multipart/form-data" in your form.
+
+
+
+
+async function uploadtest(){
+  const file = {
+    path : "uploads/img-1.jpg",
+    filename : "img-1.jpg"
+  }
+
+  let result = await uploadFile(file);
+  let url = result.Location
+  return url
+}
+
+
+async function sendImageOpenalpr(imgUrl){
+  const secret_key = process.env.SECRET_KEY
+  let url = `https://api.openalpr.com/v3/recognize_url?image_url=${imgUrl}&recognize_vehicle=0&country=us&return_image=0&topn=10&secret_key=${secret_key}`;
+  try{
+    const response = await axios.post(url)
+    console.log(response.data.results[0].plate);
+  }catch(err){
+    console.log(err);
+  }
+}
+
+// let run = async()=>{
+//   try{
+//     let AWSurl = await uploadtest();
+//     sendImageOpenalpr(AWSurl)
+//   }catch(err){
+//     console.log(err)
+//   }
+// }
+
+// run();
+
+sendImageOpenalpr('https://number-plate-recognition.s3.amazonaws.com/img-1.jpg')
 
 app.get('/', upload.single('uploaded_file'), function (req, res) {
   const file = req.file 
@@ -23,22 +62,6 @@ app.get('/', upload.single('uploaded_file'), function (req, res) {
   console.log('filename', file.filename)
   console.log('path', file.path)
 })
-
-const secret_key = process.env.SECRET_KEY
-let imgUrl = 'https://number-plate-recognition.s3.amazonaws.com/A-License-Plates-Lead-Image.jpg'
-let url = `https://api.openalpr.com/v3/recognize_url?image_url=${imgUrl}&recognize_vehicle=0&country=us&return_image=0&topn=10&secret_key=${secret_key}`;
-
-async function sendImageOpenalpr(url){
-  try{
-    const response = await axios.post(url)
-    console.log(response.data.results[0].plate);
-  }catch(err){
-    console.log(error);
-  }
-}
-
-sendImageOpenalpr(url)
-
 
 let server = app.listen(3000, () => {
   let host = server.address().address
