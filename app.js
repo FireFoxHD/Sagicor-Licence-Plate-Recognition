1c5956584dbad29f1e3f15b1a42ca758d50e7cd6
FireFoxHD <khamalipowell@gmail.com>
2022-04-26 21:56:53 -0500

    minor fixes and multer to accept file upload from front end

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-
 const express = require('express');
 const axios = require('axios').default;
 const fs = require('fs');
 const path = require ('path');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
 
 let app = express();
 
-function base64_encode(file) {
-   return "data:image/png;base64,"+fs.readFileSync(file, 'base64');
-}
+//form input needs the name 'file' to work
+//Don't forget the enctype="multipart/form-data" in your form.
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.get('/', upload.single('uploaded_file'), function (req, res) {
+  const file = req.file 
+  if(file) return res.status(200).send('Success');
+  console.log('fieldname', file.fieldname)
+  console.log('originalname', file.originalname)
+  console.log('mimetype', file.mimetype)
+  console.log('size', file.size)
+  console.log('destination', file.destination)
+  console.log('filename', file.filename)
+  console.log('path', file.path)
 })
 
-const secret_key ='sk_49a41bf53b3e366e4826f51d';
+const secret_key = process.env.SECRET_KEY
 let imgUrl = 'https://number-plate-recognition.s3.amazonaws.com/A-License-Plates-Lead-Image.jpg'
 let url = `https://api.openalpr.com/v3/recognize_url?image_url=${imgUrl}&recognize_vehicle=0&country=us&return_image=0&topn=10&secret_key=${secret_key}`;
 
-axios.post(url)
-  .then(function (response) {
+async function sendImageOpenalpr(url){
+  try{
+    const response = await axios.post(url)
     console.log(response.data.results[0].plate);
-  })
-  .catch(function (error) {
+  }catch(err){
     console.log(error);
-  });
+  }
+}
 
-
-
+sendImageOpenalpr(url)
 
 
 let server = app.listen(3000, () => {
